15417ddba9f30a11fe7aa5804bd945bfe02eab2a
Djalma Araujo <djalma.araujo@gmail.com>
2016-04-14 14:02:45 -0300

    refactoring browser action

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,19 +1,58 @@
-$(function () {
-    $("#save").on('click', function (e) {
+var SemaphoreWatcherUI = function () {
+    var instance = this;
+
+    instance.bindSaveToken();
+    instance.bindEditButton();
+    instance.syncUI();
+};
+
+SemaphoreWatcherUI.prototype.syncUI = function () {
+    var instance = this
+        , token = localStorage.getItem('semaphoreUserToken');
+
+    if ((token !== null) && (token !== "")) {
+        console.log('aa');
+        instance.showAnsweUI();
+    } else {
+        console.log('bb');
+        instance.showFormUI();
+    }
+};
+
+SemaphoreWatcherUI.prototype.bindEditButton = function () {
+    var instance = this;
+
+    $(".js-semaphore-editButton").on('click', function () {
+        instance.showFormUI();
+    });
+};
+
+SemaphoreWatcherUI.prototype.bindSaveToken = function () {
+    var instance = this;
+
+    $(".js-semaphore-saveButton").on('click', function (e) {
         e.preventDefault();
 
-        localStorage.setItem('semaphoreUserToken', $("#token").val());
-        $("#form").hide();
-        $("#answer").show();
-    });
+        localStorage.setItem('semaphoreUserToken', $(".js-semaphore-inputToken").val());
 
-    $("#edit-button").on('click', function () {
-        $("#form").show();
-        $("#answer").hide();
+        instance.syncUI();
     });
+};
 
-    if (localStorage.getItem('semaphoreUserToken') !== null) {
-        $("#form").hide();
-        $("#answer").show();
-    }
+SemaphoreWatcherUI.prototype.showFormUI = function () {
+    var instance = this;
+
+    $(".js-semaphore-formContainer").show();
+    $(".js-semaphore-answerContainer").hide();
+};
+
+SemaphoreWatcherUI.prototype.showAnsweUI = function () {
+    var instance = this;
+
+    $(".js-semaphore-formContainer").hide();
+    $(".js-semaphore-answerContainer").show();
+};
+
+$(function () {
+    new SemaphoreWatcherUI();
 });
